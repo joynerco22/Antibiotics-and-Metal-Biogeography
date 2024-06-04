076bef6e9ab804868807fd419b8deb60b530943f
joynerco22 <112597907+joynerco22@users.noreply.github.com>
2024-06-04 10:30:57 -0400

    Update download_mg_rast_data.sh

--- bin/download_mg_rast_data.sh
+++ bin/download_mg_rast_data.sh
@@ -6,9 +6,9 @@
 
 ## run API server and download to ~/MG-RAST_RAW_DATA directory 
 
-# while read line; do mg-download.py --project $line --dir ~/MG-RAST_RAW_DATA  --file 050.1; done 
-
+## sanity check to make sure the first command line argument is correct 
 echo "$1"
+## initiate while loop, which takes the ID of a library from the MG-RAST database that you provide for it in a text file. This script is compatible with all of the IDs listed in the various environmental ID files in the "Data" folder.
 while IFS= read -r id || [ -n "$id" ]; do
 id=$(echo "$id" | tr -d '\r')
 if [ -n "$id" ]; then
